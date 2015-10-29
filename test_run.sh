0791aa6f00936146d7fbc59255958e89074da328
francesco-asnicar <francesco.asnicar@gmail.com>
2015-10-29 11:47:40 +0100

    fixed the test script, now it works

--- test_run.sh
+++ test_run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
+# clean file of old runs
+rm -f boinc_lockfile stderr.txt
+
 # input data
 #   * input/experiments.csv
 #   * input/tile.txt
 
 # results stored in
 #   * output/
-
-bin/pc input/tile.txt output/output.txt 0.05 10 10
+time bin/pc input/tile.txt output/output.txt 0.05 1 393
